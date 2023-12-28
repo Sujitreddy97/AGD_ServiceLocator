1fd9a48f29d5b300331250eae02a058de878283e
Sujit Reddy <Sujitreddy97@users.noreply.github.com>
2023-12-28 11:43:57 +0530

    Using Service Locator
    -Adding all Services in the game inside Service Locator (GameService.cs)
    
    -Converting all Services into a Non-MonoBehaviour class.
    
    -Defined a Constructor for all these services.
    
    -Resolved dependencies from all of these services through GameService.
    
    -Updated references of all services by accessing them through Game Service.

--- Assets/Scripts/Main/GameService.cs
+++ Assets/Scripts/Main/GameService.cs
@@ -1,18 +1,40 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using ServiceLocator.Utilities;
 using ServiceLocator.Player;
+using ServiceLocator.Sound;
+using ServiceLocator.Map;
+using ServiceLocator.Wave;
+using ServiceLocator.UI;
+using ServiceLocator.Events;
 
 public class GameService: GenericMonoSingleton<GameService>
 {
     public PlayerService PlayerService { get; private set; }
+    public SoundService SoundService { get; private set; }
+    public MapService MapService { get; private set; }
+    public WaveService WaveService { get; private set; }
+    public EventService EventService { get; private set; }
 
-    [SerializeField] public PlayerScriptableObject playerScriptableObject;
+
+    [SerializeField]private UIService uIService;
+    public UIService UIService => uIService;
+
+
+    [SerializeField] private PlayerScriptableObject playerScriptableObject;
+    [SerializeField] private SoundScriptableObject soundScriptableObject;
+    [SerializeField] private MapScriptableObject mapScriptableObject;
+    [SerializeField] private WaveScriptableObject waveScriptableObject;
+    [SerializeField] private AudioSource audioEffects;
+    [SerializeField] private AudioSource backgroundMusic;
 
     private void Start()
     {
+        EventService = new EventService();
+        UIService.SubscribeToEvents();
         PlayerService = new PlayerService(playerScriptableObject);
+        SoundService = new SoundService(soundScriptableObject, audioEffects, backgroundMusic);
+        MapService = new MapService(mapScriptableObject);
+        WaveService = new WaveService(waveScriptableObject);
     }
 
     private void Update()
